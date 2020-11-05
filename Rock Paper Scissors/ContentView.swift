5a42e2cf4d37f9d044cfff2654edb268351798c7
Jacob Stanger <jacobcstanger@gmail.com>
2020-11-05 14:57:37 -0700

    init commit

---
+++ Rock Paper Scissors/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissors
+//
+//  Created by Jacob Stanger on 10/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var appMsg: String = "Lorem ipsum"
+    @State var someCount: Int = 0
+//    var myFunClass = ActionFunctions()
+    
+    var body: some View {
+        // Main Group
+        VStack {
+            Text("\(appMsg) \(someCount)").padding()
+            
+//            Button(action: onClickActions) {
+//                Text("Click me")
+//            }
+            HStack {
+                Button(action: onClickActions){
+                    Text("Rock")
+                }.padding()
+                Button(action: onClickActions){
+                    Text("Paper")
+                }.padding()
+                Button(action: onClickActions){
+                    Text("Scissors")
+                }.padding()
+            }
+            
+            Menu("Choose") {
+                Button("Rock", action: onClickActions)
+                Button("Paper", action: onClickActions)
+                Button("Scissors", action: onClickActions)
+            }
+        }
+    }
+    
+    func onClickActions() -> Void {
+        appMsg = "Hello, There!"
+        someCount = someCount + 1
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Jacob Stanger on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
